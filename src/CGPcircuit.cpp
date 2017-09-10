afa1063cc4aebf13ca011a2faf1386a498f06ae7
Daniel Kraut <xkraut03@stud.fit.vutbr.cz>
2017-09-10 23:59:40 +0300

    Simple CGP circuit representation

---
+++ src/CGPcircuit.cpp
@@ -0,0 +1,48 @@
+// CGPcircuit.cpp
+// author: Daniel Kraut
+// creation date: 10th of September, 2017
+//
+// Copyright © 2017 Daniel Kraut
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "CGPcircuit.hpp"
+
+CGPcircuit::CGPcircuit()
+{}
+
+void CGPcircuit::setRowsNumber(const int num_rows)
+{
+    num_rows_ = num_rows;
+}
+
+void CGPcircuit::setColumnsNumber(const int num_columns)
+{
+    num_columns_ = num_columns;
+}
+
+void CGPcircuit::setLBackValue(const int l_back_value)
+{
+    l_back_ = l_back_value;
+}
+
+void CGPcircuit::setInputsNumber(const int num_inputs)
+{
+    num_inputs_ = num_inputs;
+}
+
+void CGPcircuit::setOutputsNumber(const int num_outputs)
+{
+    num_outputs_ = num_outputs;
+}
---
+++ src/CGPcircuit.hpp
@@ -0,0 +1,38 @@
+// CGPcircuit.hpp
+// author: Daniel Kraut
+// creation date: 10th of September, 2017
+//
+// Copyright © 2017 Daniel Kraut
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#pragma once
+
+class CGPcircuit {
+private:
+    int num_rows_;
+    int num_columns_;
+    int l_back_;
+    int num_inputs_;
+    int num_outputs_;
+
+public:
+    CGPcircuit ();
+    void setRowsNumber(const int num_rows);
+    void setColumnsNumber(const int num_columns);
+    void setLBackValue(const int l_back_value);
+    void setInputsNumber(const int num_inputs);
+    void setOutputsNumber(const int num_outputs);
+    virtual ~CGPcircuit () = default;
+};
